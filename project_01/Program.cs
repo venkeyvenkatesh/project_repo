5cd51f915540889778d897757b86b1b260d3036b
venkey <venkeyvenkatesh885@gmail.com>
2020-09-24 16:51:42 +0530

    first commit

---
+++ project_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 1");
+        }
+    }
+}
